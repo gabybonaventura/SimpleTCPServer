9a370d94dcf6dda00f17fe97e03f0a77dc1bcdaf
KOMPUSUR\gbonaventura <gabybonaventura@hotmail.com>
2019-01-17 16:44:29 -0300

    first commit

---
+++ TCPServer/TCPServer.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace TCPServer
+{
+    partial class TCPServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.Startbutton = new System.Windows.Forms.Button();
+            this.Stopbutton = new System.Windows.Forms.Button();
+            this.textStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // textHost
+            // 
+            this.textHost.Location = new System.Drawing.Point(80, 37);
+            this.textHost.Name = "textHost";
+            this.textHost.Size = new System.Drawing.Size(136, 20);
+            this.textHost.TabIndex = 1;
+            this.textHost.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(306, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Port:";
+            // 
+            // textPort
+            // 
+            this.textPort.Location = new System.Drawing.Point(356, 37);
+            this.textPort.Name = "textPort";
+            this.textPort.Size = new System.Drawing.Size(136, 20);
+            this.textPort.TabIndex = 3;
+            this.textPort.Text = "8910";
+            // 
+            // Startbutton
+            // 
+            this.Startbutton.Location = new System.Drawing.Point(563, 33);
+            this.Startbutton.Name = "Startbutton";
+            this.Startbutton.Size = new System.Drawing.Size(75, 23);
+            this.Startbutton.TabIndex = 4;
+            this.Startbutton.Text = "Start";
+            this.Startbutton.UseVisualStyleBackColor = true;
+            this.Startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // Stopbutton
+            // 
+            this.Stopbutton.Location = new System.Drawing.Point(661, 32);
+            this.Stopbutton.Name = "Stopbutton";
+            this.Stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.Stopbutton.TabIndex = 5;
+            this.Stopbutton.Text = "Stop";
+            this.Stopbutton.UseVisualStyleBackColor = true;
+            this.Stopbutton.Click += new System.EventHandler(this.Stopbutton_Click);
+            // 
+            // textStatus
+            // 
+            this.textStatus.Location = new System.Drawing.Point(29, 94);
+            this.textStatus.Multiline = true;
+            this.textStatus.Name = "textStatus";
+            this.textStatus.Size = new System.Drawing.Size(707, 312);
+            this.textStatus.TabIndex = 6;
+            // 
+            // TCPServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textStatus);
+            this.Controls.Add(this.Stopbutton);
+            this.Controls.Add(this.Startbutton);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textHost);
+            this.Controls.Add(this.label1);
+            this.Name = "TCPServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TCPServer";
+            this.Load += new System.EventHandler(this.TCPServer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textPort;
+        private System.Windows.Forms.Button Startbutton;
+        private System.Windows.Forms.Button Stopbutton;
+        private System.Windows.Forms.TextBox textStatus;
+    }
+}
+
---
+++ TCPServer/TCPServer.cs
@@ -0,0 +1,58 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPServer
+{
+    public partial class TCPServer : Form
+    {
+        public TCPServer()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+
+        private void TCPServer_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13; //enter
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+        }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            textStatus.Invoke((MethodInvoker)delegate ()
+            {
+                textStatus.Text += e.MessageString;
+                textStatus.AppendText(Environment.NewLine);
+                
+
+                e.ReplyLine(string.Format("You said: {0}", e.MessageString));
+            });
+        }
+
+        private void Startbutton_Click(object sender, EventArgs e)
+        {
+            textStatus.Text += "Server starting...";
+            textStatus.AppendText(Environment.NewLine);
+
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(textHost.Text);
+            server.Start(ip, Convert.ToInt32(textPort.Text));
+        }
+
+        private void Stopbutton_Click(object sender, EventArgs e)
+        {
+            if (server.IsStarted)
+                server.Stop();
+        }
+    }
+}
